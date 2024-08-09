e47d50b78d45ce925727aee61e4df78bf7e952be
gil.00000032610@unicap.br <gil.00000032610@unicap.br>
2024-08-09 16:37:04 -0300

    Manipulate a thread with interruptions and using join to start dependent tasks

--- src/dev/lpa/runningthreads/Main.java
+++ src/dev/lpa/runningthreads/Main.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+        //Simulating a situation where a installation can only occur when a download thread completes its task
         System.out.println("The main thread is running!");
         try {
             System.out.println("Paused for 1 second!");
@@ -11,41 +12,67 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Thread thread = new Thread(()->{
+        Thread thread = new Thread(() -> {
             String tname = Thread.currentThread().getName();
-            System.out.println(tname+ " should take 10 dots to run.");
+            System.out.println(tname + " should take 10 dots to run.");
             for (int i = 0; i < 10; i++) {
                 System.out.print(". ");
                 try {
                     TimeUnit.MILLISECONDS.sleep(500);
-                    System.out.println("A. State = " + Thread.currentThread().getState());
                 } catch (InterruptedException e) {
                     System.out.println("\nWhoops!! " + tname + " interrupted");
-                    System.out.println("A1. State = " + Thread.currentThread().getState());
+                    Thread.currentThread().interrupt();
+                    // Java recommends that any method that catches an InterruptedException and is not
+                    //prepared to deal with it immediately, should reassert the exception.
+                    //Thread must reinterrupt itself or call interrupt on itself
                     return;
                 }
 
             }
-            System.out.println("\n"+ tname + " completed.");
+            System.out.println("\n" + tname + " completed.");
         });
-        System.out.println(thread.getName()+ " starting");
-        thread.start();
 
-        long now = System.currentTimeMillis();
-        while(thread.isAlive()){
-            System.out.println("\nWaiting for thread to complete");
+        Thread installThread = new Thread(() -> {
             try {
-                Thread.sleep(1000);
-                System.out.println("B. State = "+ thread.getState());
-                if(System.currentTimeMillis() - now > 2000){
-                    thread.interrupt();
+                for (int i = 0; i < 3; i++) {
+                    Thread.sleep(300);
+                    System.out.println("Installation Step " + (i + 1) + " is completed");
                 }
-            }catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }, "InstallThread");
+
+        Thread installMonitor = new Thread(() -> {
+            long now = System.currentTimeMillis();
+            while (thread.isAlive()) {
+                try {
+                    Thread.sleep(1000);
+                    if (System.currentTimeMillis() - now > 8000) {
+                        thread.interrupt();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        System.out.println(thread.getName() + " starting");
+        thread.start();
+        installMonitor.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (!thread.isInterrupted()) {
+            installThread.start();
+        } else {
+            System.out.println("Previous thread was interrupted, " + installThread.getName()
+                    + " can't run.");
         }
 
-        System.out.println("C. State = " + thread.getState());
 //        System.out.println("Main thread continuing here.");
 //
 //        try{
