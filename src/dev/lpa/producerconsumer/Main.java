3df08d0de72eb612fae82abe82811b2e701b011b
gil.00000032610@unicap.br <gil.00000032610@unicap.br>
2024-08-24 20:29:37 -0300

    Deadlock happened with the threads!!! Producer cannot change flag and vice versa.

--- src/dev/lpa/producerconsumer/Main.java
+++ src/dev/lpa/producerconsumer/Main.java
@@ -30,8 +30,7 @@ class MessageWriter implements Runnable {
     private MessageRepository outgoingMessage;
     private final String text = """
                     Água mole, pedra dura
-                    Tanto bate até que fura.
-            """;
+                    Tanto bate até que fura.""";
 
     public MessageWriter(MessageRepository outgoingMessage) {
         this.outgoingMessage = outgoingMessage;
@@ -50,7 +49,7 @@ class MessageWriter implements Runnable {
                 throw new RuntimeException(e);
             }
         }
-        outgoingMessage.write("Finished!!!");
+        outgoingMessage.write("Finished");
     }
 }
 
@@ -60,29 +59,45 @@ class MessageReader implements Runnable {
     private MessageRepository incomingMessage;
 
 
-    public MessageReader(MessageRepository incoming) {
+    public MessageReader(MessageRepository incomingMessage) {
         this.incomingMessage = incomingMessage;
     }
 
     @Override
     public void run() {
         Random random = new Random();
-
         String latestMessage = "";
-
         do {
 
             try {
                 Thread.sleep(random.nextInt(500, 2000));
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            };
-                latestMessage = incomingMessage.read();
+            }
+
+            latestMessage = incomingMessage.read();
             System.out.println(latestMessage);
 
-        }while (!latestMessage.equals("Finished"));
+        } while (!latestMessage.equals("Finished"));
     }
 }
 
 public class Main {
+
+    public static void main(String[] args) {
+        MessageRepository messageRepository = new MessageRepository();
+
+        Thread reader = new Thread(new MessageReader(messageRepository));
+        Thread writer = new Thread(new MessageWriter(messageRepository));
+        //This code is prone to Deadlocks: While the consumer wants to read the message, the producer wants to
+        //write a message (shared resource). Both threads can possibly be stuck in an eternal while loop!
+
+        //Thread reader could acquire the lock on the shared resource via the synchronized method. When this happens
+        //Thread writer would not be able to change the flag to false, causing a deadlock.
+
+        reader.start();
+        writer.start();
+
+
+    }
 }
